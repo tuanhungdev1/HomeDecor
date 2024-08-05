e307fedb030d16485a2aa4e44b600ce66c413455
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-08-05 17:10:42 +0700

    first commit

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages";
+import { RootLayout } from "./layouts";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
