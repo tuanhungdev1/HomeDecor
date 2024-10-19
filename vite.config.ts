87f8bdaf5892c36635ce42f92d79dc582f4ddb5b
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-10-19 22:44:58 +0700

    add alias import

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,17 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@store": path.resolve(__dirname, "./src/stores"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
     },
   },
 });
